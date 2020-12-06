59b04fb76e4eee09ad0f90d053739abd6c74ada6
Kinga <kinga.g@wp.pl>
2020-12-06 11:13:18 +0100

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import collections_training.ListTraining;
+import collections_training.SetTraining;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("ListTraining");
+        ListTraining.trainList();
+        System.out.println("setTraining");
+        SetTraining.trainSet();
     }
 }
