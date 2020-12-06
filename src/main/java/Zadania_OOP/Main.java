287cfc2dfc27ad026feaf08a1dca98209f231f71
Kinga <kinga.g@wp.pl>
2020-12-06 12:15:50 +0100

    Utworzenie w Main obiektów

---
+++ src/main/java/Zadania_OOP/Main.java
@@ -0,0 +1,9 @@
+package Zadania_OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine(12.05d, 1200, 15.25);
+        SportCar sportCar = new SportCar("Opel", "Corsa", "red", 2, engine);
+        
+    }
+}
--- src/main/java/Zadania_OOP/SportCar.java
+++ src/main/java/Zadania_OOP/SportCar.java
@@ -3,6 +3,6 @@ package Zadania_OOP;
 public class SportCar extends Car{
     public SportCar (String producer, String model, String color, Integer seatsNumber, Engine myEngine){
         super(producer, model, color, seatsNumber, myEngine);
-        
+
     }
 }
