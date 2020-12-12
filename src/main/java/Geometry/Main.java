e08537e1db9dd0e70b80fe78782cff9cb7533b22
Kinga <kinga.g@wp.pl>
2020-12-12 12:20:18 +0100

    Nowa klasa abstrakcyjna

--- src/main/java/Geometry/Main.java
+++ src/main/java/Geometry/Main.java
@@ -1,14 +1,26 @@
 package Geometry;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(3.12, 5.4);
         System.out.println(rectangle.getArea());
 
-        Triangle triangle = new Triangle(3.18, 6.19);
+        Triangle triangle = new Triangle(3.18, 2.19);
         System.out.println(triangle.getArea());
 
-        Circle circle = new Circle(23.12);
+        Circle circle = new Circle(3.12);
         System.out.println(circle.getArea());
+
+        System.out.println("Exercise 12");
+        List<Figure> figures = new LinkedList<>();
+        figures.add(rectangle);
+        figures.add(circle);
+        figures.add(triangle);
+
+        double result = AreaCounter.countArea(figures);
+        System.out.println(result);
     }
 }
